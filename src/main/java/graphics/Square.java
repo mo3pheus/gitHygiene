dc374a2ab913d8b3bfd03d56b27319572978af36
Sanket <sanket.korgaonkar@gmail.com>
2018-10-30 15:42:41 -0400

    Feature: Fixed awt repaint

--- src/main/java/graphics/Square.java
+++ src/main/java/graphics/Square.java
@@ -1,18 +1,26 @@
 package graphics;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
 
 public class Square extends JComponent {
+    private Logger logger = LoggerFactory.getLogger(Square.class);
+
     public Square(int frameWidth) {
         this.setBounds(0, 0, frameWidth, frameWidth);
     }
 
-    public void paint(Graphics g) {
+    @Override
+    public void paintComponent(Graphics g) {
         Graphics2D  g2          = (Graphics2D) g;
         Rectangle2D rectangle2D = new Rectangle2D.Double(100, 100, 200, 200);
         g2.setColor(Color.BLUE);
         g2.fill(rectangle2D);
+        logger.info("Square paint method called.");
     }
 }
