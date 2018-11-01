f95447e459ec6b0194b5a83cfac0a7db6d7897f7
Sanket <sanket.korgaonkar@gmail.com>
2018-11-01 16:41:44 -0400

    BugFix: Fixed references to deleted classes.

--- src/main/java/bootstrap/Driver.java
+++ src/main/java/bootstrap/Driver.java
@@ -1,13 +1,10 @@
 package bootstrap;
 
 import graphics.FrameGenerator;
-import graphics.SquareGenerator;
-import graphics.CircleGenerator;
 import org.apache.log4j.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.awt.*;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
